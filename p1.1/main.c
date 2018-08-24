9ec46fe9e32952d11c7706da1a0dca67539ffaf9
xsy <xsy@xsy.local>
2018-08-24 12:06:22 -0400

    update1.1

---
+++ p1.1/main.c
@@ -0,0 +1,19 @@
+//
+//  main.c
+//  p1.1
+//
+//  Created by xsy on 8/24/18.
+//  Copyright © 2018 xsy. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("%d\n",1+2);
+    printf("%d\n",4%2);
+    printf("%d\n",5/2);
+    printf("%d\n",5*7);
+    printf("%d\n",4-8);
+    return 0;
+}
---
+++ p1.2/main.c
@@ -0,0 +1,19 @@
+//
+//  main.c
+//  p1.2
+//
+//  Created by xsy on 8/24/18.
+//  Copyright © 2018 xsy. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    // .2f means 2 decimal places
+    // integer/integer = integer; float/float = float;
+    printf("%.2f\n",13.0/4.0);
+    printf("%.2f\n",13/4);
+    printf("%d\n",13.0/4.0);
+    return 0;
+}
